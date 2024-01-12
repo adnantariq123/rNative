0b1aa5d9c81bdd45a8b9776e0cb1df654df6c4c8
adnantariq123 <ahmedadnan123@hotmail.com>
2024-01-12 15:13:04 -0500

    flex to take up remainder space

--- FirstRun/App.js
+++ FirstRun/App.js
@@ -4,13 +4,13 @@ import Box from "./assets/Pages/Box";
 export default function App() {
   return (
     <View style={styler.container}>
-      <Box  propsStyle={{backgroundColor: "#aaa"}}> Box 1</Box>
-      <Box  propsStyle={{backgroundColor: "#999"}}> Box 2</Box>
-      <Box  propsStyle={{backgroundColor: "#777"}}> Box 3</Box>
-      <Box  propsStyle={{backgroundColor: "#555"}}> Box 4</Box>
-      <Box  propsStyle={{backgroundColor: "#333"}}> Box 5</Box>
-      <Box  propsStyle={{backgroundColor: "#111"}}> Box 6</Box>
-      <Box  propsStyle={{backgroundColor: "#000"}}> Box 7</Box>
+      <Box  propsStyle={{backgroundColor: "#aaa", flex:1}}> Box 1</Box>
+      <Box  propsStyle={{backgroundColor: "#999", flex:1}}> Box 2</Box>
+      <Box  propsStyle={{backgroundColor: "#777", flex:1}}> Box 3</Box>
+      <Box  propsStyle={{backgroundColor: "#555", flex:1}}> Box 4</Box>
+      <Box  propsStyle={{backgroundColor: "#333", flex:1}}> Box 5</Box>
+      <Box  propsStyle={{backgroundColor: "#111", flex:1}}> Box 6</Box>
+      <Box  propsStyle={{backgroundColor: "#000", flex:1}}> Box 7</Box>
     </View>
   );
 }
@@ -20,6 +20,7 @@ const styler = StyleSheet.create({
     flex:1,
     marginTop:20,
     borderWidth:2,
-    borderColor: "red"
+    borderColor: "red",
+    //overflow:"hidden"
   },
 });
