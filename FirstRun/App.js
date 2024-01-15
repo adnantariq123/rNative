2eb758aab3c8639dae3f8c4295d07baa5040e25e
adnantariq123 <ahmedadnan123@hotmail.com>
2024-01-15 10:06:15 -0500

    style get updated if using inline style instead

--- FirstRun/App.js
+++ FirstRun/App.js
@@ -8,8 +8,9 @@ export default function App() {
   })
 
   useEffect(()=>{
-    //alert("this should also work")
+    //
     const subscription = Dimensions.addEventListener("change", ({window})=>{
+      alert("this will work when the orientation is changes, BUT it does not seem to update the styles. a refresh is required... not cool :(")
       setDimensions ({window})
     })
     return ()=> subscription?.remove();
@@ -20,10 +21,17 @@ export default function App() {
   const windowWidth = window.width;
   const windowHeight = window.height;
 
+//alert(windowWidth) // iPhone width is 667, iPad width is 810
+//alert(windowHeight)// iPhone height is 375, iPad height is 1080
+// HOWEVER this changes from landscapr to protraite modes!!
+
   return (
     <View style={styler.container}>
       <View style={styler.box}>
-        <Text style={styler.text}>Hello Rafi!</Text>
+        {/* <Text style={styler.text}>Hello Rafi!</Text> */}
+        {/*  this DOES WORK!- notice the letter psacing getting updated. however the iPads WIDTH is more than 500 in 
+        both landscape as well as protrait mode so it seems as if nothing had changed */}
+        <Text style={{fontSize:30, letterSpacing: windowWidth > 500 ? 20: 0}}>Hello Rafi!</Text>
       </View>
     </View>
   );
@@ -32,7 +40,7 @@ export default function App() {
 // Screen and window dimensions are different on android
 const windowWidth=Dimensions.get("window").width // screen refering to the entire thing.. 
 const windowHeight=Dimensions.get("window").height
-//ONE VERY MAJOR DRAW BACK of the deminsions api -  the values do not dynamiclly update,
+//ONE VERY MAJOR DRAW BACK of the deminsions api -  the values do not dynamiclly update, for eg: teh responsive style dont seem to be update?
 // when the windows deminsions change either due to orentation, or more complex senarios as foldable phones
 
 // alert(windowWidth) 
@@ -56,9 +64,9 @@ const styler = StyleSheet.create({
     alignItems: "center",
     justifyContent:"center"
   },
-  text: {
-    fontSize:30,
-    fontWeight:"bold",
-    letterSpacing: windowWidth > 500 ? 20: 0 // so this letterSpacing:20 will only show on the iPad JUST HOW COOL is that!
-  }
+  // text: {
+  //   fontSize:30,
+  //   fontWeight:"bold",
+  //   letterSpacing: windowWidth > 500 ? 20: 0 // so this letterSpacing:20 will only show on the iPad JUST HOW COOL is that!
+  // }
 });
